08c57f73a9bb04206b9581f89dc15937c7b2734f
piensoho@gmail.com <piensoho@gmail.com>
2025-05-15 10:17:50 +0200

    cf converter first try

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+// translate fahrenheit to celsius
+float fahrenheit_to_celsius (float f){
+    float c = (f-32.0) * 5.0 /9.0;
 
-// This is where you add the two functions
+    return c;
+    
+}
+
+// translate celsius to fahrenheit
+float celsius_to_fahrenheit (float c) {
+    float f = (c*9.0) / 5.0 + 32.0;
+
+    return f;
+}
 
 int main() {
     float temp_f = 100.0;
